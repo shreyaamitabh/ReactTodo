00f9ffcdc4066256154c4cbfb9e41c34795685a9
Shreya <shreya.amitabh@gmail.com>
2020-07-07 12:22:12 +0530

    Updates

---
+++ precache-manifest.3fb67eeec00fdc1c32aa499eb880cf27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3128746ee9460488ede8dbe0e883dc",
+    "url": "/ReactTodo/index.html"
+  },
+  {
+    "revision": "a730a746c8beaad13f29",
+    "url": "/ReactTodo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f5f55e7fe8eb0e20b8a3",
+    "url": "/ReactTodo/static/css/main.02a40bac.chunk.css"
+  },
+  {
+    "revision": "a730a746c8beaad13f29",
+    "url": "/ReactTodo/static/js/2.4b40e316.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactTodo/static/js/2.4b40e316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f55e7fe8eb0e20b8a3",
+    "url": "/ReactTodo/static/js/main.a9fb2891.chunk.js"
+  },
+  {
+    "revision": "ef2692b95bbb614b258a",
+    "url": "/ReactTodo/static/js/runtime-main.2d20e36b.js"
+  }
+]);
